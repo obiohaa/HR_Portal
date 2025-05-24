7db5f0951a857258bcd71db47dec8ddd219595c3
obiohaa <obiohaebuka200215@gmail.com>
2025-05-25 00:10:10 +0100

    bio data

---
+++ client/hr-admin/src/Components/UserData.jsx
@@ -0,0 +1,262 @@
+const stateCapital = [
+  {
+    id: 0,
+    state: "Select a state",
+    value: "",
+    capital: "Umuahia",
+    isDisabled: true,
+  },
+  {
+    id: 1,
+    state: "Abia",
+    value: "Abia",
+    capital: "Umuahia",
+  },
+  {
+    id: 2,
+    state: "Adamawa",
+    value: "Adamawa",
+    capital: "Yola",
+  },
+  {
+    id: 3,
+    state: "Akwa Ibom",
+    value: "Akwa Ibom",
+    capital: "Uyo",
+  },
+  {
+    id: 4,
+    state: "Anambra",
+    value: "Anambra",
+    capital: "Awka",
+  },
+  {
+    id: 5,
+    state: "Bauchi",
+    value: "Bauchi",
+    capital: "Bauchi",
+  },
+  {
+    id: 6,
+    state: "Bayelsa",
+    value: "Bayelsa",
+    capital: "Yenagoa",
+  },
+  {
+    id: 7,
+    state: "Benue",
+    value: "Benue",
+    capital: "Makurdi",
+  },
+  {
+    id: 8,
+    state: "Borno",
+    value: "Borno",
+    capital: "Maiduguri",
+  },
+  {
+    id: 9,
+    state: "Cross River",
+    value: "Cross River",
+    capital: "Calabar",
+  },
+  {
+    id: 10,
+    state: "Delta",
+    value: "Delta",
+    capital: "Asaba",
+  },
+  {
+    id: 11,
+    state: "Ebonyi",
+    value: "Ebonyi",
+    capital: "Abakaliki",
+  },
+  {
+    id: 12,
+    state: "Edo",
+    value: "Edo",
+    capital: "Benin City",
+  },
+  {
+    id: 13,
+    state: "Ekiti",
+    value: "Ekiti",
+    capital: "Ado-Ekiti",
+  },
+  {
+    id: 14,
+    state: "Enugu",
+    value: "Enugu",
+    capital: "Enugu",
+  },
+  {
+    id: 15,
+    state: "Gombe",
+    value: "Gombe",
+    capital: "Gombe",
+  },
+  {
+    id: 16,
+    state: "Imo",
+    value: "Imo",
+    capital: "Owerri",
+  },
+  {
+    id: 17,
+    state: "Jigawa",
+    value: "Jigawa",
+    capital: "Dutse",
+  },
+  {
+    id: 18,
+    state: "Kaduna",
+    value: "Kaduna",
+    capital: "Kaduna",
+  },
+  {
+    id: 19,
+    state: "Kano",
+    value: "Kano",
+    capital: "Kano",
+  },
+  {
+    id: 20,
+    state: "Kastina",
+    value: "Kastina",
+    capital: "Katsina",
+  },
+  {
+    id: 21,
+    state: "Kebi",
+    value: "Kebi",
+    capital: "Birnin Kebbi",
+  },
+  {
+    id: 22,
+    state: "Kogi",
+    value: "Kogi",
+    capital: "Lokoja",
+  },
+  {
+    id: 23,
+    state: "Kwara",
+    value: "Kwara",
+    capital: "Ilorin",
+  },
+  {
+    id: 24,
+    state: "Lagos",
+    value: "Lagos",
+    capital: "Ikeja",
+  },
+  {
+    id: 25,
+    state: "Nasarawa",
+    value: "Nasarawa",
+    capital: "Lafia",
+  },
+  {
+    id: 26,
+    state: "Niger",
+    value: "Niger",
+    capital: "Minna",
+  },
+  {
+    id: 27,
+    state: "Ogun",
+    value: "Ogun",
+    capital: "Abeokuta",
+  },
+  {
+    id: 28,
+    state: "Ondo",
+    value: "Ondo",
+    capital: "Akure",
+  },
+  {
+    id: 29,
+    state: "Osun",
+    value: "Osun",
+    capital: "Osogbo",
+  },
+  {
+    id: 30,
+    state: "Oyo",
+    value: "Oyo",
+    capital: "Ibadan",
+  },
+  {
+    id: 31,
+    state: "Plateau",
+    value: "Plateau",
+    capital: "Jos",
+  },
+  {
+    id: 32,
+    state: "Rivers",
+    value: "Rivers",
+    capital: "Port Harcourt",
+  },
+  {
+    id: 33,
+    state: "Sokoto",
+    value: "Sokoto",
+    capital: "Sokoto",
+  },
+  {
+    id: 34,
+    state: "Taraba",
+    value: "Taraba",
+    capital: "Jalingo",
+  },
+  {
+    id: 35,
+    state: "Yobe",
+    value: "Yobe",
+    capital: "Damaturu",
+  },
+  {
+    id: 36,
+    state: "Zamfara",
+    value: "Zamfara",
+    capital: "Gusau",
+  },
+  {
+    id: 37,
+    state: "Abuja",
+    value: "Abuja",
+    capital: "Abuja",
+  },
+];
+
+const genderOptions = [
+  { id: 0, gender: "Gender", value: "" },
+  { id: 1, gender: "Male", value: "Male" },
+  { id: 2, gender: "Female", value: "Female" },
+  { id: 3, gender: "Others", value: "Others" },
+];
+
+const maritalStatus = [
+  { id: 0, status: "Marital Status", value: "" },
+  { id: 1, status: "Single", value: "Single" },
+  { id: 2, status: "Married", value: "Married" },
+  { id: 3, status: "Divorced", value: "Divorced" },
+];
+
+const levelOfEducation = [
+  { id: 1, level: "Primary" },
+  { id: 2, level: "Secondary" },
+  { id: 3, level: "Trade or Business School" },
+  { id: 4, level: "OND Or Equivalent" },
+  { id: 5, level: "Bachelors Or Equivalent" },
+  { id: 5, level: "Masters Or Equivalent" },
+  { id: 5, level: "Doctorate Or Equivalent" },
+];
+
+const pension = [
+  { id: 1, pension: "No, i don't have a pension account" },
+  { id: 2, pension: "Yes, i have a pension account" },
+];
+
+export { stateCapital, genderOptions, maritalStatus, levelOfEducation, pension };
--- client/hr-admin/src/Pages/Body/User/BioData.jsx
+++ client/hr-admin/src/Pages/Body/User/BioData.jsx
@@ -1,6 +1,500 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import {
+  stateCapital,
+  genderOptions,
+  maritalStatus,
+  pension,
+  levelOfEducation,
+} from "../../../Components/UserData";
+// import { useForm } from "react-hook-form";
 
 const BioData = () => {
-  return <div className="bioDataContainer">BIO DATA</div>;
+  const [marriageStatus, setMarriageStatus] = useState("");
+  const [pensionStatus, setPensionStatus] = useState("");
+
+  const {
+    register,
+    reset,
+    formState: { errors, isSubmitting },
+    handleSubmit,
+    setError,
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleChange = (event) => {
+    setMarriageStatus(event.target.value);
+  };
+
+  const handlePensionChange = (event) => {
+    setPensionStatus(event.target.value);
+  };
+
+  return (
+    <div className="bioDataContainer">
+      <form action="" className="formContainer" autoComplete="off" noValidate>
+        <div className="formBioData">
+          <input
+            type="text"
+            id="firstName"
+            className="form_input"
+            placeholder=" "
+            autoComplete="off"
+            formNoValidate
+            {...register("firstName", {
+              required: "First Name is required!",
+              minLength: {
+                value: 2,
+                message: "Minimum characters of 2 letters.",
+              },
+              maxLength: {
+                value: 20,
+                message: "Maximum characters of 20 letters.",
+              },
+              pattern: {
+                value: /^[A-Za-z]+$/i,
+                message: "Alphabets only!",
+              },
+            })}
+          />
+          {errors.firstName && <p className="error">{errors.firstName.message}</p>}
+          <label htmlFor="firstName" className="form_label">
+            First Name
+          </label>
+        </div>
+        <div className="formBioData">
+          <input
+            type="text"
+            id="middleName"
+            className="form_input"
+            placeholder=" "
+            autoComplete="off"
+            formNoValidate
+            {...register("middleName", {
+              required: "Middle Name is required!",
+              minLength: {
+                value: 2,
+                message: "Minimum characters of 2 letters.",
+              },
+              maxLength: {
+                value: 20,
+                message: "Maximum characters of 20 letters.",
+              },
+              pattern: {
+                value: /^[A-Za-z]+$/i,
+                message: "Alphabets only!",
+              },
+            })}
+          />
+          {errors.middleName && <p className="error">{errors.middleName.message}</p>}
+          <label htmlFor="middleName" className="form_label">
+            Middle Name
+          </label>
+        </div>
+        <div className="formBioData">
+          <input
+            type="text"
+            id="lastName"
+            className="form_input"
+            placeholder=" "
+            autoComplete="off"
+            formNoValidate
+            {...register("lastName", {
+              required: "Last Name is required!",
+              minLength: {
+                value: 2,
+                message: "Minimum characters of 2 letters.",
+              },
+              maxLength: {
+                value: 20,
+                message: "Maximum characters of 20 letters.",
+              },
+              pattern: {
+                value: /^[A-Za-z]+$/i,
+                message: "Alphabets only!",
+              },
+            })}
+          />
+          {errors.lastName && <p className="error">{errors.lastName.message}</p>}
+          <label htmlFor="lastName" className="form_label">
+            Last Name
+          </label>
+        </div>
+        <div className="formBioData">
+          <input
+            type="date"
+            id="dateOfBirth"
+            className="form_input"
+            placeholder=" "
+            autoComplete="off"
+            formNoValidate
+            {...register("dateOfBirth", {
+              required: "Date of Birth is required!",
+              validate: {
+                isFutureDate: (value) => new Date(value) < new Date() || "Date must be in the past",
+              },
+            })}
+          />
+          {errors.date && <p className="error">{errors.date.message}</p>}
+          <label htmlFor="dateOfBirth" className="form_label">
+            Date of Birth
+          </label>
+        </div>
+        <div className="formBioData selectFormBioData">
+          <select
+            name="state_of_origin"
+            id="state_of_origin"
+            className="form_input"
+            autoComplete="off"
+            {...register("state_of_origin", {
+              required: "State of Origin is required!",
+              validate: {
+                isValidState: () =>
+                  stateCapital.some((state) => state.state === state.value) ||
+                  "Invalid state selected",
+              },
+            })}>
+            {stateCapital.map((state) => (
+              <option key={state.id} value={state.state}>
+                {state.state}
+              </option>
+            ))}
+          </select>
+          {errors.state_of_origin && <p className="error">{errors.state_of_origin.message}</p>}
+          <label htmlFor="dateOfBirth" className="form_label">
+            State of Origin
+          </label>
+        </div>
+        <div className="formBioData selectFormBioData">
+          <select
+            name="gender"
+            id="gender"
+            className="form_input"
+            {...register("gender", {
+              required: "Gender is required!",
+              validate: {
+                isValidState: () =>
+                  genderOptions.some((gender) => gender.gender === gender.value) ||
+                  "Invalid gender selected",
+              },
+            })}>
+            {genderOptions.map((gender) => {
+              return (
+                <option key={gender.id} value={gender.gender}>
+                  {gender.gender}
+                </option>
+              );
+            })}
+          </select>
+          {errors.gender && <p className="error">{errors.gender.message}</p>}
+          <label htmlFor="gender" className="form_label">
+            Gender
+          </label>
+        </div>
+        <div className="formBioData selectFormBioData">
+          <select
+            name="maritalStatus"
+            id="maritalStatus"
+            className="form_input"
+            {...register("maritalStatus", {
+              required: "Marital status is required!",
+              validate: {
+                isValidState: () =>
+                  maritalStatus.some(
+                    (maritalStatus) => maritalStatus.status === maritalStatus.value
+                  ) || "Invalid status selected",
+              },
+            })}
+            onChange={handleChange}>
+            {maritalStatus.map((status) => {
+              return (
+                <option key={status.id} value={status.status}>
+                  {status.status}
+                </option>
+              );
+            })}
+          </select>
+          {errors.maritalStatus && <p className="error">{errors.maritalStatus.message}</p>}
+          <label htmlFor="gender" className="form_label">
+            Marital Status
+          </label>
+        </div>
+        {marriageStatus === "Married" && (
+          <div className="formBioData">
+            <input
+              type="text"
+              id="spouseName"
+              name="spouseName"
+              className="form_input"
+              placeholder=" "
+              autoComplete="off"
+              {...register("spouseName", {
+                required: "Spouse name is required!",
+                minLength: {
+                  value: 2,
+                  message: "Minimum characters of 2 letters.",
+                },
+                maxLength: {
+                  value: 20,
+                  message: "Maximum characters of 20 letters.",
+                },
+                pattern: {
+                  value: /^[A-Za-z]+$/i,
+                  message: "Alphabets only!",
+                },
+              })}
+            />
+            {errors.spouseName && <p className="error">{errors.spouseName.message}</p>}
+            <label htmlFor="spouseName" className="form_label">
+              Spouse Name
+            </label>
+          </div>
+        )}
+        <div className="formBioData">
+          <input
+            type="text"
+            id="houseAddress"
+            name="houseAddress"
+            className="form_input"
+            placeholder=" "
+            autoComplete="off"
+            {...register("houseAddress", {
+              required: "House address is required!",
+              minLength: {
+                value: 3,
+                message: "Minimum characters of 2 letters.",
+              },
+              maxLength: {
+                value: 100,
+                message: "Maximum characters of 20 letters.",
+              },
+            })}
+          />
+          {errors.houseAddress && <p className="error">{errors.houseAddress.message}</p>}
+          <label htmlFor="houseAddress" className="form_label">
+            House Address
+          </label>
+        </div>
+        <div className="formBioData">
+          <input
+            type="number"
+            id="phoneNumber"
+            name="phoneNumber"
+            className="form_input"
+            placeholder=" "
+            autoComplete="off"
+            {...register("phoneNumber", {
+              required: "Phone number is required!",
+              minLength: {
+                value: 10,
+                message: "Minimum characters of 2 letters.",
+              },
+              maxLength: {
+                value: 14,
+                message: "Maximum characters of 20 letters.",
+              },
+              pattern: {
+                value: /^[0-9]*$/,
+                message: "Numbers only!",
+              },
+            })}
+          />
+          {errors.phoneNumber && <p className="error">{errors.phoneNumber.message}</p>}
+          <label htmlFor="phoneNumber" className="form_label">
+            Phone Number
+          </label>
+        </div>
+        <div className="formBioData">
+          <input
+            name="email"
+            type="email"
+            id="emailAddress"
+            className="form_input"
+            placeholder=" "
+            autoComplete="off"
+          />
+          <label htmlFor="emailAddress" className="form_label">
+            Email Address
+          </label>
+        </div>
+        <div className="formBioData">
+          <input
+            type="text"
+            id="bankName"
+            name="bankName"
+            className="form_input"
+            placeholder=" "
+            autoComplete="off"
+            {...register("bankName", {
+              required: "Bank name is required!",
+              minLength: {
+                value: 10,
+                message: "Minimum characters of 2 letters.",
+              },
+              maxLength: {
+                value: 30,
+                message: "Maximum characters of 20 letters.",
+              },
+              pattern: {
+                value: /^[A-Za-z]+$/i,
+                message: "Alphabets only!",
+              },
+            })}
+          />
+          {errors.bankName && <p className="error">{errors.bankName.message}</p>}
+          <label htmlFor="bankName" className="form_label">
+            Bank Name
+          </label>
+        </div>
+
+        <div className="formBioData">
+          <input
+            type="number"
+            id="bankAccountNumber"
+            className="form_input"
+            placeholder=" "
+            autoComplete="off"
+            {...register("bankAccountNumber", {
+              required: "Bank account number is required!",
+              minLength: {
+                value: 11,
+                message: "Minimum characters of 11 letters.",
+              },
+              maxLength: {
+                value: 11,
+                message: "Maximum characters of 11 letters.",
+              },
+              pattern: {
+                value: /^[0-9]*$/,
+                message: "Numbers only!",
+              },
+            })}
+          />
+          {errors.bankAccountNumber && <p className="error">{errors.bankAccountNumber.message}</p>}
+          <label htmlFor="bankAccountNumber" className="form_label">
+            Bank Account Number
+          </label>
+        </div>
+        <div className="formBioData selectFormBioData">
+          <select
+            name="pension"
+            id="pension"
+            className="form_input"
+            {...register("pension")}
+            onChange={handlePensionChange}>
+            {pension.map((pen) => {
+              return (
+                <option key={pen.id} value={pen.pension}>
+                  {pen.pension}
+                </option>
+              );
+            })}
+          </select>
+          <label htmlFor="gender" className="form_label">
+            pension
+          </label>
+        </div>
+        {pensionStatus === "Yes, i have a pension account" && (
+          <div className="formBioData">
+            <input
+              type="text"
+              id="pensionCompany"
+              name="pensionCompany"
+              className="form_input"
+              placeholder=" "
+              autoComplete="off"
+              {...register("pensionCompany", {
+                required: "Pension Company is required!",
+                minLength: {
+                  value: 2,
+                  message: "Minimum characters of 2 letters.",
+                },
+                maxLength: {
+                  value: 30,
+                  message: "Maximum characters of 20 letters.",
+                },
+                pattern: {
+                  value: /^[A-Za-z]+$/i,
+                  message: "Alphabets only!",
+                },
+              })}
+            />
+            {errors.pensionCompany && <p className="error">{errors.pensionCompany.message}</p>}
+            <label htmlFor="pensionCompany" className="form_label">
+              Pension Company
+            </label>
+          </div>
+        )}
+        {pensionStatus === "Yes, i have a pension account" && (
+          <div className="formBioData">
+            <input
+              type="number"
+              id="pensionPin"
+              name="pensionPin"
+              className="form_input"
+              placeholder=" "
+              autoComplete="off"
+              {...register("pensionPin", {
+                required: "Pension PIN number is required!",
+                minLength: {
+                  value: 5,
+                  message: "Minimum characters of 5 letters.",
+                },
+                maxLength: {
+                  value: 20,
+                  message: "Maximum characters of 20 letters.",
+                },
+                pattern: {
+                  value: /^[0-9]*$/,
+                  message: "Numbers only!",
+                },
+              })}
+            />
+            {errors.pensionPin && <p className="error">{errors.pensionPin.message}</p>}
+            <label htmlFor="pensionPin" className="form_label">
+              Pension Pin
+            </label>
+          </div>
+        )}
+        <div className="formBioData selectFormBioData">
+          <select name="levelOfEducation" id="pension" className="form_input">
+            {levelOfEducation.map((edu) => {
+              return (
+                <option key={edu.id} value={edu.level}>
+                  {edu.level}
+                </option>
+              );
+            })}
+          </select>
+          <label htmlFor="levelOfEducation" className="form_label">
+            Level of Education
+          </label>
+        </div>
+        <div className="formBioData">
+          <input
+            type="file"
+            id="passportImg"
+            className="form_input"
+            placeholder=" "
+            autoComplete="off"
+          />
+          <label htmlFor="passportImg" className="form_label">
+            Upload Passport Image
+          </label>
+        </div>
+        <div className="formBioData">
+          <input type="file" id="cv" className="form_input" placeholder=" " autoComplete="off" />
+          <label htmlFor="cv" className="form_label">
+            Upload CV
+          </label>
+        </div>
+      </form>
+    </div>
+  );
 };
 export default BioData;
